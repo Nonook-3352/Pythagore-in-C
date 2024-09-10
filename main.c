c1f77345fd6ae2df5024a278cc4311ac6660cf53
Nonook-3352 <ggnonook@gmail.com>
2024-09-10 18:54:38 +0200

    feat: main code

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+float AB, BC, AC;
+
+float pythagore(int ab, int bc) {
+	double result;
+	result = ab*ab + bc*bc;
+	result = sqrt(result);
+
+	return result;
+};
+
+int main() {
+	printf("AB ");
+	scanf("%f", &AB);
+	printf("\nBC ");
+	scanf("%f", &BC);
+	printf("\nRésultat : %lf\n", pythagore(AB, BC));
+	
+	return 0;
+}
